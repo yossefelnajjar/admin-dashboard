9da81931485a9ecfc4f7126f74e06aca0390ebc2
yossef elnajjar <yossefelnajjar@gmail.com>
2023-04-04 10:53:43 +0200

    finishing projects page

---
+++ src/components/projects/ProjectsBox.js
@@ -0,0 +1,45 @@
+import ProgressBar from "../ProgressBar";
+import DashboardBtn from "../dashboard/DashboardBtn";
+
+const ProjectsBox = ({
+  title,
+  description,
+  date,
+  children,
+  tech1,
+  tech2,
+  tech3,
+  tech4,
+  price,
+  progress,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between items-center mb-16">
+        <div className="flex-1">
+          <h3 className="text-xl">{title}</h3>
+          <p className="text-sm text-slate-400">{description}</p>
+        </div>
+        <span className="text-slate-300">{date}</span>
+      </div>
+      <div className="">{children}</div>
+      <div className="py-3 border-y border-slate-400 my-2 flex justify-end">
+        <DashboardBtn content={tech1} bgColor="mr-2" />
+        <DashboardBtn content={tech2} bgColor="mr-2" />
+        <DashboardBtn content={tech3} bgColor="mr-2" />
+        <DashboardBtn content={tech4} />
+      </div>
+      <div className="flex justify-center items-center">
+        <ProgressBar
+          innerBg="bg-blue-900"
+          outerBg="bg-blue-500"
+          outerW="w-full"
+          innerW={progress}
+        />
+        <span className="ml-5 text-orange-300">{price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsBox;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,36 @@
+import { team1, team2 } from "../assets/assets";
 import Wrapper from "../components/Wrapper";
+import ProjectsBox from "../components/projects/ProjectsBox";
 
 const Projects = () => {
-  return (
-    <Wrapper head="dashboard">
-      <div className="rounded-lg p-4 bg-slate-800">test</div>
-      <div className="rounded-lg p-4 bg-slate-800">test</div>
-      <div className="rounded-lg p-4 bg-slate-800">test</div>
-      <div className="rounded-lg p-4 bg-slate-800">test</div>
-      <div className="rounded-lg p-4 bg-slate-800">test</div>
-    </Wrapper>
-  );
+  const boxes = [];
+
+  for (let i = 0; i < 10; i++) {
+    boxes.push(
+      <div key={i} className="rounded-lg p-4 bg-slate-800">
+        <ProjectsBox
+          title="Academy Portal"
+          description="Academy Portal Project Design And Programming"
+          date="15/2/2023"
+          tech1="Programming"
+          tech2="Design"
+          tech3="Hosting"
+          tech4="Marketing"
+          price="$25,000"
+          progress="w-4/5">
+          <div className="flex">
+            <img src={team1} className="projects-team" alt="team member" />
+            <img src={team2} className="projects-team" alt="team member" />
+            <img src={team1} className="projects-team" alt="team member" />
+            <img src={team1} className="projects-team" alt="team member" />
+            <img src={team2} className="projects-team" alt="team member" />
+          </div>
+        </ProjectsBox>
+      </div>
+    );
+  }
+
+  return <Wrapper head="dashboard">{boxes}</Wrapper>;
 };
 
 export default Projects;
